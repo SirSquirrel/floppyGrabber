626f9cf1709001424b881bbd2c4a6aaf854ab0b4
JonasTarnowetzki <jjt739@mail.usask.ca>
2015-01-31 17:52:36 -0600

    Baby commit
    Mostly catchup, also has some babies.

---
+++ project/Assets/scripts/CharSelect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharSelect : MonoBehaviour {
+
+	ArrayList<int> selectBox;
+	int currentBox;
+
+	// Use this for initialization
+	void Start () {
+		selectBox = new ArrayList (2);
+		currentBox = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Return)) {
+
+		}
+	}
+}
--- project/Assets/scripts/MainMenuUI.cs
+++ project/Assets/scripts/MainMenuUI.cs
@@ -5,11 +5,15 @@ public class MainMenuUI : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.anyKeyDown) {
+			AutoFade.LoadLevel ("CharSelect", 3, 1, Color.black);
+		}
 	}
+
+
 }
