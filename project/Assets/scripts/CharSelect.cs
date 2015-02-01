c7e2cfb305a4b3b0213f944d80434a8c6084636b
JonasTarnowetzki <jjt739@mail.usask.ca>
2015-01-31 21:15:12 -0600

    Doing some work with character selection.
    Needs the pirate prefab.

--- project/Assets/scripts/CharSelect.cs
+++ project/Assets/scripts/CharSelect.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * 
+ * 
+ */
+
 public class CharSelect : MonoBehaviour {
 
-	ArrayList selectBox;
-	int currentBox;
+	GameObject p1_selector_1;
+	GameObject p2_selector_1;
+	GameObject p1_selector_2;
+	GameObject p2_selector_2;
+
+	GameObject whiteT_P1;
+	GameObject whiteT_P2;
+
+	Character[] selectBox_p1;
+	Character[] selectBox_p2;
+	int currentBox_p1;
+	int currentBox_p2;
+
+	public class Character {
+		
+		public GameObject selector;
+		public GameObject character;
+
+		public Character(GameObject sel, GameObject cha) {
+			this.selector = sel;
+			this.character = cha;
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
-		selectBox = new ArrayList (2);
-		currentBox = 0;
+		selectBox_p1 = new Character[2];
+		selectBox_p2 = new Character[2];
+		currentBox_p1 = 0;
+		currentBox_p2 = 0;
+
+		selectBox_p1[0] = new Character(
+			p1_selector_1.GetComponent<Sprite>(), whiteT_P1.GetComponent<Sprite>());
 	}
 	
 	// Update is called once per frame
@@ -17,5 +48,30 @@ public class CharSelect : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Return)) {
 
 		}
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			if (currentBox_p1 == 0) {
+				currentBox_p1 = selectBox.Capacity - 1;
+			}
+			else
+			{
+				currentBox_p1--;
+			}
+			this.ScreenRefresh1();
+		}
+
+
+	}
+
+	/*
+	 * A method to update the screen after a player has changed 
+	 * their character selection.
+	 */
+	void ScreenRefresh1() {
+		foreach (Character c in selectBox_p1) {
+			c.character.renderer.enabled = false;
+			c.selector.renderer.enabled = false;
+		}
+		selectBox_p1 [currentBox_p1].selector.renderer.enabled = true;
+		selectBox_p1 [currentBox_p1].character.renderer.enabled = true;
 	}
 }
