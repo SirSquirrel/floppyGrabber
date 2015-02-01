70db6fab9b3f9824ae6b3eb8c2172afe8c8b933c
Squirrell <ksquires11@gmail.com>
2015-02-01 13:31:01 -0800

    levelSelect

---
+++ project/Assets/SceneChangeOnClick.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChangeOnClick : MonoBehaviour {
+
+	public int sceneNumber = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnMouseDown()
+	{
+	Application.LoadLevel(sceneNumber);
+	}
+}
